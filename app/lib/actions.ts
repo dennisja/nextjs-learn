2098a19026d1206bd6789503a8f042008735413e
dennisja <dennisjjagwe@gmail.com>
2023-11-29 22:41:47 +0100

    create an invoice using server actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+const InvoiceSchema = z.object({
+  id: z.string().uuid(),
+  customerId: z.string().uuid(),
+  amount: z.coerce.number().positive(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoiceFormSchema = InvoiceSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoiceFormSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  await sql`INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
